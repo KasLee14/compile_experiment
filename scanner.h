3a63b247b57b6d271979395ee362e5f884db5013
Max-chy <1501928404@qq.com>
2022-12-20 21:33:37 +0800

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -27,26 +27,7 @@ struct Token
 };
 
 //区分记号的符号表
-struct Token TokenTab[] = {
-        {CONST_ID, "PI", 3.1415926, NULL},
-        {CONST_ID, "E", 2.71828, NULL},
-        {T, "T",	 0.0, NULL},
-        {FUNC, "SIN", 0.0, sin},
-        {FUNC, "COS", 0.0, cos},
-        {FUNC, "TAN", 0.0, tan},
-        {FUNC, "LN", 0.0, log},
-        {FUNC, "EXP", 0.0, exp},
-        {FUNC, "SQRT", 0.0, sqrt},
-        {ORIGIN, "ORIGIN", 0.0, NULL},
-        {SCALE, "SCALE", 0.0, NULL},
-        {ROT, "ROT", 0.0, NULL},
-        {IS, "IS", 0.0, NULL},
-        {FOR, "FOR", 0.0, NULL},
-        {FROM, "FROM", 0.0, NULL},
-        {TO, "TO", 0.0, NULL},
-        {STEP, "STEP", 0.0, NULL},
-        {DRAW, "DRAW", 0.0, NULL}
-};
+extern struct Token TokenTab[]；
 
 const int token_len = 100;
 
