9405d836e917139535d7c189b4fd058676dcae99
Max-chy <1501928404@qq.com>
2022-12-20 21:34:02 +0800

    Update scanner.cpp

--- scanner.cpp
+++ scanner.cpp
@@ -1,5 +1,27 @@
 #include "scanner.h"
 
+//区分记号的符号表
+struct Token TokenTab[] = {
+        {CONST_ID, "PI", 3.1415926, NULL},
+        {CONST_ID, "E", 2.71828, NULL},
+        {T, "T",	 0.0, NULL},
+        {FUNC, "SIN", 0.0, sin},
+        {FUNC, "COS", 0.0, cos},
+        {FUNC, "TAN", 0.0, tan},
+        {FUNC, "LN", 0.0, log},
+        {FUNC, "EXP", 0.0, exp},
+        {FUNC, "SQRT", 0.0, sqrt},
+        {ORIGIN, "ORIGIN", 0.0, NULL},
+        {SCALE, "SCALE", 0.0, NULL},
+        {ROT, "ROT", 0.0, NULL},
+        {IS, "IS", 0.0, NULL},
+        {FOR, "FOR", 0.0, NULL},
+        {FROM, "FROM", 0.0, NULL},
+        {TO, "TO", 0.0, NULL},
+        {STEP, "STEP", 0.0, NULL},
+        {DRAW, "DRAW", 0.0, NULL}
+};
+
 //初始化词法分析器，成功时返回非0，失败返回0
 int scanner::InitScanner(const char *fileName) {
 
